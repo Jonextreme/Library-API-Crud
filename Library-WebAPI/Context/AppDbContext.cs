c9a308593b7273a3de836b21be2edc4d1b31eb02
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-02 14:49:42 -0300

    Added Constructor to class AppDbContext

--- Library-WebAPI/Context/AppDbContext.cs
+++ Library-WebAPI/Context/AppDbContext.cs
@@ -12,6 +12,10 @@ namespace Library_WebAPI.Context
         public DbSet<Loan> Loans { get; set; }
         public DbSet<User> Users { get; set; }
 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new AuthorConfiguration());
