8a4fb727556d424d6c9924624f97ba5e62f9309c
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-04 19:02:34 -0300

    Added Telephone property to UserCreateDTO

--- Library-WebAPI/DTOs/UserDTOs/UserCreateDTO.cs
+++ Library-WebAPI/DTOs/UserDTOs/UserCreateDTO.cs
@@ -5,6 +5,7 @@ namespace Library_WebAPI.DTOs.UserDTOs
     public class UserCreateDTO
     {
         public string Name { get; set; } = "";
+        public string Telephone { get; set; } = "";
         public string? Email { get; set; }
         public DateTime Birthdate { get; set; }
     }
