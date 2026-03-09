b4ccf86d69c9a3e0a48843f6368640804fec67cf
Jonathan Moreno <jonathanwillianvidalmoreno@gmail.com>
2026-03-09 17:34:10 -0300

    Add dotnet user-secrets to keep database connection string

--- Library-WebAPI/Program.cs
+++ Library-WebAPI/Program.cs
@@ -6,13 +6,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Configuration.AddUserSecrets<Program>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-string? sqlServerConnectionString = builder.Configuration.GetConnectionString("MyDatabaseConnectionString");
+string? sqlServerConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(sqlServerConnectionString));
 
